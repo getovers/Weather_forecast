5e382267b1293fc143b14a58f55de26003eba5ec
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2023-01-14 19:05:22 +0200

    create func to fetch and get weather forecast

--- js/dataServive.js
+++ js/dataServive.js
@@ -1,8 +1,16 @@
 class DataService{
-    #url = 'http://api.openweathermap.org/data/2.5/';
+    #url = 'http://api.openweathermap.org/data/2.5/weather';
     #appId = 'bf35cac91880cb98375230fb443a116f';
 
-    getWeatherForecast(cityId){
+    async getWeatherForecast(cityId){
+        const url = `${this.#url}?id=${cityId}&appid=${this.#appId}`;
+        const response = await fetch(url);
+        if(response.ok){
+            return response.json();
+        }
+        else {
+            throw new Error(`Status: ${response.status}, message: ${response.statusText}`);
+        }
     }
 }
 export const dataService = new DataService();
--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 import { ContentSwitcher } from './contentSwitcher.js';
+import { STATE } from './state.js';
+import { Forecast } from './forecast.js';
+import { dataService } from './dataServive.js';
 
 const contentSwitcher = new ContentSwitcher();
 
-window.addEventListener('load', () =>{
+window.addEventListener('load', async() =>{
     contentSwitcher.showContent();
+    STATE.citiesList.forEach( async cityId => {
+        const responseObj = await dataService.getWeatherForecast(cityId);
+        const currentForecast = new Forecast(responseObj);
+        
+    });
     contentSwitcher.disablePreloader();
 } )
